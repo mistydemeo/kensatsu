50d1b10413d7726254e925ac30777161f7ba3400
Misty De Meo <mistydemeo@gmail.com>
2021-05-25 22:31:42 -0700

    Implement the backoff logic

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,46 @@
 use std::collections::HashMap;
 use std::process::exit;
+use std::time::Instant;
 
 use pafe::pasori;
 
+fn should_back_off(timestamp: Instant) -> bool {
+    if Instant::now().duration_since(timestamp).as_secs() >= 1 {
+        false
+    } else {
+        true
+    }
+}
+
 fn read_loop(pasori: pasori::Pasori) -> Result<(), String> {
     let mut hm = HashMap::new();
+    // Used to track when a card is lifted
+    // Note that all zeroes is not a valid IDm, so we can safely
+    // use that as a "null" value.
+    let mut last_read = [0; 8];
     loop {
         if let Some(felica_tag) = pasori.poll(pasori::CardType::Any, pasori::Timeslot::N0) {
             let idm = felica_tag.tag.IDm;
-            if !hm.contains_key(&idm) {
-                hm.insert(idm, true);
-                println!("Scanned card {:02x?}", idm);
+            // If the last card scanned was this card, do nothing.
+            // `last_read` should be the null IDm if the card was lifted.
+            // Protects against repeatedly acting on the same card while
+            // it's sitting on the reader.
+            if last_read == idm {
+                continue;
             }
+
+            // Decide if enough time has passed since the last read
+            if let Some(backoff_timetamp) = hm.get(&idm) {
+                if should_back_off(*backoff_timetamp) {
+                    continue;
+                }
+            }
+            println!("Scanned card {:02x?}", idm);
+
+            last_read = idm;
+        } else {
+            hm.insert(last_read, Instant::now());
+            last_read = [0; 8];
         }
     }
 }
