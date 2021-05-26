68d21ebe2e9c4d7baf7fe867c384a11060fbee23
Misty De Meo <mistydemeo@gmail.com>
2021-05-25 22:09:29 -0700

    First teeny read loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::collections::HashMap;
+use std::process::exit;
+
+use pafe::pasori;
+
+fn read_loop(pasori: pasori::Pasori) -> Result<(), String> {
+    let mut hm = HashMap::new();
+    loop {
+        if let Some(felica_tag) = pasori.poll(pasori::CardType::Any, pasori::Timeslot::N0) {
+            let idm = felica_tag.tag.IDm;
+            if !hm.contains_key(&idm) {
+                hm.insert(idm, true);
+                println!("Scanned card {:02x?}", idm);
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let pasori = pasori::Pasori::create();
+    match pasori.init() {
+        Ok(_) => {},
+        Err(_) => {
+            eprintln!("No PaSoRi detected!");
+            exit(1);
+        },
+    }
+    read_loop(pasori).unwrap();
+
+    exit(0);
 }
