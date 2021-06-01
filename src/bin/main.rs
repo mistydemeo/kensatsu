8b10c77a1cefeba02aa8a4acded6d407bd236188
Misty De Meo <mistydemeo@gmail.com>
2021-05-31 22:44:12 -0700

    Main executable: use EVDevEmitter on Linux

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,6 @@
 use kensatsu::emitter::IOEmitter;
+#[cfg(target_os = "linux")]
+use kensatsu::emitter::EVDevEmitter;
 use kensatsu::reader::read_loop;
 
 use std::process::exit;
@@ -7,6 +9,10 @@ use pafe::pasori;
 
 fn main() {
     if let Some(pasori) = pasori::Pasori::create() {
+        // TODO: make the IOEmitter an option on Linux
+        #[cfg(target_os = "linux")]
+        let mut emitter = EVDevEmitter { device: Device::create().unwrap() };
+        #[cfg(not(target_os = "linux"))]
         let mut emitter = IOEmitter { target: &mut std::io::stdout() };
 
         read_loop(pasori, &mut emitter).unwrap();
